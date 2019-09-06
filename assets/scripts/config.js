6342e78682bd541dedf918fc692a035f3a89d510
dtersoff <daverboy21@gmail.com>
2019-09-06 09:58:50 -0400

    Put links in README

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://dtersoff-full-stack-project.herokuapp.com/',
+  production: 'https://blooming-everglades-15045.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
