1c3f3b48066177010fa2a595eb6c7bbac481aed0
dtersoff <daverboy21@gmail.com>
2019-09-06 09:42:14 -0400

    Commit before deploy

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://dtersoff-full-stack-project.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
