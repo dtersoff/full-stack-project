7ed935c2c1eb40ef3feb4f4dbf182e4a8e4972eb
dtersoff <daverboy21@gmail.com>
2019-09-16 14:08:38 -0400

    Add comment

--- assets/scripts/templates/helpers/selector.js
+++ assets/scripts/templates/helpers/selector.js
@@ -2,9 +2,7 @@
 const store = require('../../store')
 
 const selector = (dropValue) => {
-  // console.log(dropValue)
-  // console.log(store.currentServant.sclass)
-  // console.log(store.sort.normalString(dropValue) === store.sort.normalString(store.currentServant.sclass))
+  // TODO: figure out why the strings from the seed data aren't matching the strings from the stored array
   if (typeof dropValue === 'number') {
     return dropValue === store.currentServant.rarity
   } else {
