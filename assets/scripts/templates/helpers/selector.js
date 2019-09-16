a87bab1366296c4a2dbc0d3739f6d3a14dbb8ca9
dtersoff <daverboy21@gmail.com>
2019-09-16 14:04:59 -0400

    Fix servant class update dropdown
    The selector was returning false even when the classes matched for some
    unknown reason. Using the string normalizing method from the sorter made
    it work properly, but need to look into what's wrong to fix the problem
    at the source.

--- assets/scripts/templates/helpers/selector.js
+++ assets/scripts/templates/helpers/selector.js
@@ -2,10 +2,13 @@
 const store = require('../../store')
 
 const selector = (dropValue) => {
+  // console.log(dropValue)
+  // console.log(store.currentServant.sclass)
+  // console.log(store.sort.normalString(dropValue) === store.sort.normalString(store.currentServant.sclass))
   if (typeof dropValue === 'number') {
     return dropValue === store.currentServant.rarity
   } else {
-    return dropValue === store.currentServant.sclass
+    return store.sort.normalString(dropValue) === store.sort.normalString(store.currentServant.sclass)
   }
 }
 
